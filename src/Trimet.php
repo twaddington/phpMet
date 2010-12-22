12bcae920fb653f394145f91bf7b0f92141ba0e0
Tristan Waddington <tristan.waddington@gmail.com>
2010-12-21 22:23:28 -0800

    Added default pacific time zone.

--- src/Trimet.php
+++ src/Trimet.php
@@ -1,5 +1,12 @@
 <?php
 
+date_default_timezone_set('America/Los_Angeles');
+
+require_once('TrimetDatetime.php');
+require_once('TrimetLocation.php');
+require_once('TrimetArrival.php');
+require_once('TrimetAPIQuery.php');
+
 /**
  * Stub Trimet base class
  */
